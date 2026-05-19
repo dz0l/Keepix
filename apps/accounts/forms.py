3bbd47b03d94a501053dad01bed01eee6092f783
dz0l <7344764@gmail.com>
2026-05-19 20:22:18 +0300

    1.2.4

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -2,5 +2,14 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Логин', max_length=150)
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    username = forms.CharField(
+        label='Логин',
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'input', 'autocomplete': 'username'}),
+    )
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(
+            attrs={'class': 'input', 'autocomplete': 'current-password'},
+        ),
+    )
